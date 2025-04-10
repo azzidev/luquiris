10040d5cc7ba80af7edc0e37bde4b0172de0e184
Matheus Azzi <mazzic.dev@gmail.com>
2025-04-10 20:34:43 -0300

    set ts and tsx configs

---
+++ frontend-react/src/presentation/pages/components/header/Header-Config.ts
@@ -0,0 +1,37 @@
+const menuNavItems = [
+    {
+      key: "Product",
+      label: `Product`,
+      children: [
+        {
+          key: 'services-1',
+          label: 'Plugin Comments',
+        },
+      ],
+    },
+    {
+      key: "explore",
+      label: "Explore",
+      children: [
+        {
+          key: 'explore-1',
+          label: '',
+        }
+      ]
+    },
+    {
+      key: "prices",
+      label: "Prices",
+    },
+    {
+      key: "login",
+      label: "Login",
+    },
+    {
+      key: "playground",
+      label: "Playground",
+    },
+  ];
+  
+  export default menuNavItems;
+  
